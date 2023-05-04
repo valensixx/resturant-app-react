e708a43d38a80be4b16f5a4f56a5ade8677cfe3c
valensixx <valensixx@gmail.com>
2023-05-04 14:24:44 +0300

    connecting components in one file

---
+++ src/components/indexComponents.js
@@ -0,0 +1,13 @@
+import Navbar from './Navbar/Navbar';
+import Menuitem from './Menuitem/Menuitem';
+import Subheading from './SubHeading/Subheading';
+import Newsletter from './Footer/Newsletter';
+import Footer from './Footer/Footer';
+
+export {
+    Navbar,
+    Menuitem,
+    Subheading,
+    Newsletter,
+    Footer,
+};
