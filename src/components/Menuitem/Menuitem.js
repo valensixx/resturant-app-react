5be0d3bdd1e088e8693895dfc89d8b79e685423a
valensixx <valensixx@gmail.com>
2023-05-04 14:14:43 +0300

    creating file structure for the project

---
+++ src/components/Menuitem/Menuitem.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "Menuitem.css";
+
+export default function Menuitem(){
+    return(
+        <div>
+            Menuitem
+        </div>
+    );
+}
---
+++ src/components/SubHeading/Subheading.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "Subheading.css";
+
+export default function Subheading(){
+    return(
+        <div>
+            Subheading
+        </div>
+    );
+}
