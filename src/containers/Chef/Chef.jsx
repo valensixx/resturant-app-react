a4282b3f72007470da5d49d21386bd5fa926da3b
valensixx <valensixx@gmail.com>
2023-05-12 12:37:06 +0300

    working on container elements

---
+++ src/containers/Chef/Chef.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "Chef.css";
+
+export default function Chef(){
+    return(
+        <div>
+            Chef
+        </div>
+    );
+}
---
+++ src/containers/FindUs/FindUs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "FindUs.css";
+
+export default function FindUs(){
+    return(
+        <div>
+            FindUs
+        </div>
+    );
+}
