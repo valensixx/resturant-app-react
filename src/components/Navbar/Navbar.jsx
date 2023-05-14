75f05af6eb455dfc33a6ee511e3f8e17262cc746
valensixx <valensixx@gmail.com>
2023-05-14 13:17:29 +0300

    start working on navigation menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Navbar.css";
 
+//here i will inport icons and images later on the project
+//import {HamburgerMenu} from 'react-icons/';
+//import {OutlineResturantMenu} from 'react-icons/';
+//import images from '../../constants/images';
+
 export default function Navbar(){
     return(
-        <div>
+        <nav className="app-navbar">
             Navbar
         
-        </div>
+        </nav>
     );
 }
