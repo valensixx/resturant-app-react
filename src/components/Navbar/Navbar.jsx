f6a5625ca12ef62f986ad6c7e371d181f05b6b0a
valensixx <valensixx@gmail.com>
2023-05-14 15:28:43 +0300

    install font aswome and working on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,36 @@
 import React from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 //here i will inport icons and images later on the project
-
+//Need to work on toggle menu later
 //<div className="app-navbar-smallscreen">
 //<HamburgerMenu color="#fff" fontsize={27} onClick={()=>{}}/>               
 //</div>  
 
-export default function Navbar(){
-    return(
+export default function Navbar() {
+    return (
         <nav className="app-navbar">
             <div className="app-navbar-logo">
                 <img src={logo} alt="app logo" />
-            </div> 
+            </div>
             <ul className="app-navbar-links">
                 <li className="p-opensans"><a href="#home">Home</a></li>
                 <li className="p-opensans"><a href="#about">About</a></li>
                 <li className="p-opensans"><a href="#menu">Menu</a></li>
                 <li className="p-opensans"><a href="#awards">Awards</a></li>
                 <li className="p-opensans"><a href="#contacts">Contacts</a></li>
-            </ul>  
+            </ul>
             <div className="app-navbar-login">
                 <a href="#login" className="p-opensans">Log In / Register</a>
                 <div />
                 <a href="/" className="p-opensans">Table booking</a>
-            </div>  
+            </div>
+            <div className="app-navbar-smallscreen">
+               
+            </div>
+
         </nav>
     );
 }
