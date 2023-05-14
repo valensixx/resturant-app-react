0b77a55edf54484b5a9d94139f264f8b9f35df28
valensixx <valensixx@gmail.com>
2023-05-14 13:51:24 +0300

    working on jsx structure and some css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,8 +9,21 @@ import "./Navbar.css";
 export default function Navbar(){
     return(
         <nav className="app-navbar">
-            Navbar
-        
+            <div className="app-navbar-logo">
+                <img src="{images.logo}" alt="app logo" />
+            </div> 
+            <ul className="app-navbar-links">
+                <li className="p-opensans"><a href="#home">Home</a></li>
+                <li className="p-opensans"><a href="#about">About</a></li>
+                <li className="p-opensans"><a href="#menu">Menu</a></li>
+                <li className="p-opensans"><a href="#awards">Awards</a></li>
+                <li className="p-opensans"><a href="#contacts">Contacts</a></li>
+            </ul>  
+            <div className="app-navbar-login">
+                <a href="#login" className="p-opensans">Log In / Register</a>
+                <div />
+                <a href="/" className="p-opensans">Table booking</a>
+            </div>    
         </nav>
     );
 }
