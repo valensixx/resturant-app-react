ead8f73e6445b70f7bd5f26680ae4efdd0dccad1
valensixx <valensixx@gmail.com>
2023-05-16 15:18:49 +0300

    clone missing files from git to PC

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import images from '../../data/images';
 
-export default function Navbar(){
-    return(
-        <div>
-            Navbar
-        
-        </div>
+
+const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    return (
+        <nav className="app-navbar">
+            <div className="app-navbar-logo">
+                <img src={logo} alt="app logo" />
+            </div>
+            <ul className="app-navbar-links">
+                <li className="p-opensans"><a href="#home">Home</a></li>
+                <li className="p-opensans"><a href="#about">About</a></li>
+                <li className="p-opensans"><a href="#menu">Menu</a></li>
+                <li className="p-opensans"><a href="#awards">Awards</a></li>
+                <li className="p-opensans"><a href="#contacts">Contacts</a></li>
+            </ul>
+            <div className="app-navbar-login">
+                <a href="#login" className="p-opensans">Log In / Register</a>
+                <div />
+                <a href="/" className="p-opensans">Table booking</a>
+            </div>
+            <div className="app-navbar-smallscreen">
+                <GiHamburgerMenu color="#fff" fontsize={27} onClick={() => setToggleMenu(true)} />
+
+                {toggleMenu && (
+                    <div className="app-navbar-smallscreen-overlay flex-center slide-bottom">
+                        <MdOutlineRestaurantMenu fontSize={27} className="overlay-close" onClick={() => setToggleMenu(false)} />
+                        <ul className="app-navbar-smallscrean-links">
+                            <li className="p-opensans"><a href="#home">Home</a></li>
+                            <li className="p-opensans"><a href="#about">About</a></li>
+                            <li className="p-opensans"><a href="#menu">Menu</a></li>
+                            <li className="p-opensans"><a href="#awards">Awards</a></li>
+                            <li className="p-opensans"><a href="#contacts">Contacts</a></li>
+                        </ul>
+                    </div>
+                )}
+            </div>
+
+        </nav>
     );
-}
+};
+
+export default Navbar;
