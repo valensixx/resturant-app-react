222c4d44ad9130fce4009866e00ff81dacfb896e
valensixx <valensixx@gmail.com>
2023-05-17 17:48:33 +0300

    fixed display errors

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Header.css";
 import {images} from '../../constants';
-import {SubHeading} from '../../components';
+import {Subheading} from '../../components';
 
 export default function Header(){
     return(
         <div className="app-header app-wrapper section-padding" id="home">
             <div className="app-wrapper-info">
-                <SubHeading />
+                <Subheading />
             </div>
 
             <div className="app-wrapper-img">
