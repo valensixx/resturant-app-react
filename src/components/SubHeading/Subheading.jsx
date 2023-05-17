1f0940c2b4ed83822dbfc3d97c2f75761c1d7e67
valensixx <valensixx@gmail.com>
2023-05-17 17:46:34 +0300

    Header.jsx and Subheading.jsx

--- src/components/SubHeading/Subheading.jsx
+++ src/components/SubHeading/Subheading.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Subheading.css";
+import {images} from '../../constants';
 
 export default function Subheading(){
     return(
-        <div>
-            Subheading
+        <div style ={{marginBottom: '1rem'}}>
+            <p className="p-cormorant">TITLE</p>
+            <img src={images.spoon} alt="spoon image" className="spoon-img" />
         </div>
     );
 }
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./Header.css";
+import {images} from '../../constants';
+import {SubHeading} from '../../components';
 
 export default function Header(){
     return(
-        <div>
-            Header
-        
+        <div className="app-header app-wrapper section-padding" id="home">
+            <div className="app-wrapper-info">
+                <SubHeading />
+            </div>
+
+            <div className="app-wrapper-img">
+
+            </div>
         </div>
     );
 }
