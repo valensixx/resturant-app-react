eaaf041f8da617908291b2d09957b8f377a8890d
valensixx <valensixx@gmail.com>
2023-05-17 16:48:13 +0300

    navbar.css done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Navbar from './components/Navbar/Navbar';
 export default function App(){
     return (
         <div>
-            <Navbar />
+           <Navbar />
             <Header />
             <AboutUs />
             <Menu />
