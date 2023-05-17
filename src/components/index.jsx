8eb125e6e91e9b8c9ec7bca64cc559a5f4eee91c
valensixx <valensixx@gmail.com>
2023-05-17 18:41:27 +0300

    cant fix SubHeading error - will redo it

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,13 +1,11 @@
 import Navbar from './Navbar/Navbar';
 import Menuitem from './Menuitem/Menuitem';
-import Subheading from './Subheading/';
 import Newsletter from './Footer/Newsletter';
 import Footer from './Footer/Footer';
 
 export {
     Navbar,
     Menuitem,
-    Subheading,
     Newsletter,
     Footer,
 };
