72dc496276ea9de935cd42f9cca4dde23cf218a7
valensixx <valensixx@gmail.com>
2023-05-18 14:59:56 +0300

    SubHeading component

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,11 @@ import Navbar from './Navbar/Navbar';
 import Menuitem from './Menuitem/Menuitem';
 import Newsletter from './Footer/Newsletter';
 import Footer from './Footer/Footer';
+import SubHeading from './SubHeading/SubHeading';
 
 export {
     Navbar,
+    SubHeading,
     Menuitem,
     Newsletter,
     Footer,
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Header.css";
-import { images } from "../../data/images";
+import { images } from "../../constants";
 
 
 export default function Header(){
