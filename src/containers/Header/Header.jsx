e64cdaf258bd0e32198d9903f1959f7be2d4b55a
valensixx <valensixx@gmail.com>
2023-05-18 18:14:04 +0300

    changes

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -3,23 +3,19 @@ import "./Header.css";
 import images from '../../data/images';
 import { SubHeading } from "../../components";
 
-
-export default function Header(){
-    return(
-        <div className="app-header app-wrapper section-padding" id="home">
-            <div className="app-wrapper-info">
-                <SubHeading title="Chase the new flavour" />
-                <h1 className="app-header-h1">The Key To Fine Dining </h1>
-                <p className="p-opensans" style={{margin:'2rem 0'}}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                </p>
-                <button type="button" className="custom-button">Explore Menu</button>
-            </div>
-
-            <div className="app-wrapper-img">
-                <img src={images.welcome} alt="welcome image"/>
-            </div>
-        </div>
-    );
-}
+const Header = () => (
+    <div className="app-header app-wrapper section-padding" id="home">
+      <div className="app__wrapper_info">
+        <SubHeading title="Chase the new flavour" />
+        <h1 className="app-header-h1">The Key To Fine Dining</h1>
+        <p className="p-opensans" style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+        <button type="button" className="custom-button">Explore Menu</button>
+      </div>
+  
+      <div className="app-wrapper-img">
+        <img src={images.welcome} alt="header-img" />
+      </div>
+    </div>
+  );
+  
+  export default Header;
