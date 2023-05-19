a5e66a5e3460ebe82409f9f6f4301f705e7b4c8d
valensixx <valensixx@gmail.com>
2023-05-19 18:43:23 +0300

    working on Menu.jsx

--- src/containers/Menu/Menu.jsx
+++ src/containers/Menu/Menu.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import "./Menu.css";
 import {images, data} from '../../data';
+import { SubHeading, Menuitem } from "../../components";
+
+
 export default function Menu(){
     return(
-        <div>
-            Menu
-        </div>
+        <div className="app-specialMenu flex-center section-padding" id="menu">
+            <div className="app-specialMenu-title">
+                <SubHeading title="Menu That Fits Your Pallate"/>
+                <h1 className="headtext-cormorant">Today's Special</h1>
+            </div>
+
+            <div className="app-menu-menu">
+                <div className="app-menu-menu-wine flex-center">
+                    <p className="app-menu-menu-heading">Wine & Beer</p>
+                    <div className="app-menu-menu-items">
+                        {data.wines.map((wine, index)=>(
+                            <p>{wine.title}</p>
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+        </div>        
     );
 }
