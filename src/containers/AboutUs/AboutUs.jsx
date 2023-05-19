f6236e438b56ff71d32b80a28e909cdefd43a820
valensixx <valensixx@gmail.com>
2023-05-19 13:33:19 +0300

    working on AboutUs.jsx

--- src/containers/AboutUs/AboutUs.jsx
+++ src/containers/AboutUs/AboutUs.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./AboutUs.css";
+import images from '../../data/images';
 
 export default function AboutUs(){
     return(
-        <div>
-            AboutUs
+        <div className="app-aboutus app-bg flex-center section-padding" id="about">
+            <div className="app-aboutus-overlay flex-center">
+                <img src={images.V} alt="V letter"/>
+            </div>
+
+            <div className="app-aboutus-content flex-center">
+                <div className="app-aboutus-content-about">
+                    <h1 className="headtext-comorant">About Us</h1>
+                    <img src={images.spoon} alt="spoon" className="spoon-img"/>
+                    <p className="p-opensans">
+                        Lorem Ipsum has been the industry's standard dummy text ever 
+                        since the 1500s, when an unknown printer took a galley of type 
+                        and scrambled it to make a type specimen book.
+                    </p>
+                    <button type="button" className="custom-button">Know More</button>
+                </div>
+            </div>
         </div>
     );
 }
