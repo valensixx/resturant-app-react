d7375966b00fee18586eb25c1c3bad6d844cba26
valensixx <valensixx@gmail.com>
2023-05-22 17:13:27 +0300

    working on Menu and Menuitem components and css

--- src/components/Menuitem/Menuitem.jsx
+++ src/components/Menuitem/Menuitem.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "./Menuitem.css";
 
-export default function Menuitem(){
+export default function Menuitem({title,price,tags}){
     return(
-        <div>
-            Menuitem
+        <div className="app.menuitem">
+            <div className="app-menuitem-head">
+                <div className="app-menuitem-name">
+                    <p className="p-cormorant" style={{color:'#DCCA87'}}>{title}</p>
+                </div>
+
+                <div className="app-menuitem-dash" />
+
+                <div className="app-menuitem-price">
+                    <p className="p-cormorant">{price}</p>
+                </div>
+            </div>
+
+            <div className="app-menuitem-sub">
+                <p className="p-opensans" style={{color:'#AAA'}}>{tags}</p>
+            </div>
         </div>
     );
 }
--- src/containers/Menu/Menu.jsx
+++ src/containers/Menu/Menu.jsx
@@ -12,12 +12,12 @@ export default function Menu(){
                 <h1 className="headtext-cormorant">Today's Special</h1>
             </div>
 
-            <div className="app-menu-menu">
+            <div className="app-specialmMenu-menu">
                 <div className="app-menu-menu-wine flex-center">
                     <p className="app-menu-menu-heading">Wine & Beer</p>
                     <div className="app-menu-menu-items">
                         {data.wines.map((wine, index)=>(
-                            <p>{wine.title}</p>
+                            <Menuitem key={wine.title + index} title={wine.title} price={wine.price} tags ={wine.tags}/>
                         ))}
                     </div>
                 </div>
@@ -27,14 +27,14 @@ export default function Menu(){
                 <div className="app-menu-menu-cocktails flex-center">
                     <p className="app-menu-menu-heading">cocktails & Beer</p>
                     <div className="app-menu-menu-items">
-                        {data.cocktails.map((cocktails, index)=>(
-                            <p>{cocktails.title}</p>
+                        {data.cocktails.map((cocktail, index)=>(
+                            <Menuitem key={cocktail.title} price={cocktail.price} title={cocktail.price} tags ={cocktail.tags}/>
                         ))}
                     </div>
                 </div>
             </div>
             <div style={{marginTop:'15px'}}>
-                <button type="button" className="custom-button">View more</button>            
+                <button type="button" className="custom-button"> View more </button>            
             </div>
         </div>        
     );
