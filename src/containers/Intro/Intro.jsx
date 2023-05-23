9d345dea36f83fc1808a689df8ea0b1e7efd66a8
valensixx <valensixx@gmail.com>
2023-05-23 14:18:28 +0300

    video working on logics

--- src/containers/Intro/Intro.jsx
+++ src/containers/Intro/Intro.jsx
@@ -1,11 +1,20 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./Intro.css";
 import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 import meal from '../../assets/meal.mp4';
 import { data } from "../../data";
 
 export default function Intro() {
+    const [playVideo, setPlayVideo] = useState(false);
     const vidRef = useRef();
+    const handleVideo = () => {
+        setPlayVideo((prevPlayVideo) => !prevPlayVideo)
+        if (playVideo) {
+            vidRef.current.pause();
+        } else {
+            vidRef.current.play();
+        }
+    }
 
     return (
         <div className="app-video">
@@ -16,6 +25,19 @@ export default function Intro() {
                 type="video/mp4"
                 loop
             />
+            <div className="app-video-overlay flex-center">
+                <div
+                    className="app-video-overlay-circle flexcenter"
+                    onClick={handleVideo}
+                >
+                    {playVideo ?
+                        (
+                            <BsPauseFill color="#fff" fontSize={30} />
+                        ) : <BsFillPlayFill color="#fff" fontSize={30} />
+                    }
+
+                </div>
+            </div>
         </div>
     );
 }
