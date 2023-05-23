d8e797f039b56a037ff9cc2f9c05c4b33100fe46
valensixx <valensixx@gmail.com>
2023-05-23 14:06:17 +0300

    Intro.jsx

--- src/containers/Intro/Intro.jsx
+++ src/containers/Intro/Intro.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Intro.css";
 import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 import meal from '../../assets/meal.mp4';
 import { data } from "../../data";
 
 export default function Intro() {
+    const vidRef = useRef();
+
     return (
         <div className="app-video">
-            <video src={meal} controls />
+            <video
+                src={meal}
+                ref={vidRef}
+                controls={false}
+                type="video/mp4"
+                loop
+            />
         </div>
     );
 }
