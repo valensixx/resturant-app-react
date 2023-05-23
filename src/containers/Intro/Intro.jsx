2eac8a0f0930e8e8a47e2cb81f6a5f6d8f82aeb8
valensixx <valensixx@gmail.com>
2023-05-23 13:44:51 +0300

    Intro.jsx

--- src/containers/Intro/Intro.jsx
+++ src/containers/Intro/Intro.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Intro.css";
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import meal from '../../assets/meal.mp4';
+import { data } from "../../data";
 
-export default function Intro(){
-    return(
-        <div>
-            Intro
+export default function Intro() {
+    return (
+        <div className="app-video">
+            <video src={meal} controls />
         </div>
     );
 }
