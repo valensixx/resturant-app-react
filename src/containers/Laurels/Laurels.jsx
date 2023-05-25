ea1b67e9cf840a0c91763943099499edc23a0be3
valensixx <valensixx@gmail.com>
2023-05-25 12:16:18 +0300

    Laurels.jsx structure

--- src/containers/Laurels/Laurels.jsx
+++ src/containers/Laurels/Laurels.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Laurels.css";
+import { images, data } from '../../data';
+import { SubHeading } from "../../components";
 
 export default function Laurels(){
     return(
-        <div>
-            Laurels
+        <div className="bg-app app-wrapper section-padding" id="awards">
+            <div className="app-wrapper-info">
+                
+            </div>
+
+            <div className="app-wrapper-img">
+
+            </div>
         </div>
     );
 }
